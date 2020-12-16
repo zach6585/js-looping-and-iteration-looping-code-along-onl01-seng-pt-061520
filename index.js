5e5a481f21f732c98bb4ecac312c097bf7b0052f
zach6585 <zmines@wesleyan.edu>
2020-12-15 21:10:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let array = [];
+    for (let i = 0; i < 3; i++ )
+    {
+        
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        console.log(message);
+        array.push(message);
+        
+    }
+    names = array;
+    return names;
+}
+
+function countDown(num) {
+    while (num != -1) {
+        console.log(num);
+        num--;
+    }
+}
